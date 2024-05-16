23913168662dee561943806c6b4f06e4426c614b
naz <nyuksek17@ku.edu.tr>
2024-05-16 15:24:48 +0300

    feat: success bottom sheet implemented

---
+++ src/screens/AddNewAddressScreen/components/SuccessBottomSheet.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import Modal from 'react-native-modal';
+import {StyleSheet, Text, View, ViewStyle} from 'react-native';
+import SuccessCircle from '../../../assets/svgs/SuccessCircle';
+import {
+  scaleHeight,
+  scaleWidth,
+  SCREEN_WIDTH,
+} from '../../../utils/DimensionEditor';
+import useScreenBottomDistance from '../../../hooks/useScreenBottomDistance';
+
+export type ModalRefProps = {
+  isVisible: boolean;
+  onModalHide?: () => void;
+};
+
+const SuccessBottomSheet: React.FC<ModalRefProps> = props => {
+  const {isVisible, onModalHide} = props;
+  const paddingBottom = useScreenBottomDistance();
+
+  const padding: ViewStyle = {
+    paddingBottom: scaleHeight(32) + paddingBottom,
+    paddingTop: scaleHeight(56),
+  };
+  return (
+    <Modal
+      isVisible={isVisible}
+      style={styles.modal}
+      coverScreen={false}
+      backdropColor="rgba(24, 17, 64, 0.3)"
+      backdropOpacity={1}>
+      <View style={[styles.modalView, padding]}>
+        <View style={styles.circleContainer}>
+          <SuccessCircle color="#019693" />
+        </View>
+        <Text style={styles.modalText}>Adresin başarıyla kaydedildi!</Text>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modal: {
+    margin: 0,
+  },
+  modalView: {
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: '#FFFFFF',
+    width: SCREEN_WIDTH,
+    paddingVertical: scaleHeight(32),
+    alignItems: 'center',
+    borderTopRightRadius: scaleWidth(16),
+    borderTopLeftRadius: scaleWidth(16),
+  },
+  modalText: {
+    fontWeight: '500',
+    fontSize: scaleHeight(16),
+    lineHeight: scaleHeight(20),
+    marginTop: scaleHeight(16),
+  },
+  circleContainer: {
+    padding: scaleWidth(16),
+    backgroundColor: '#E3F7F7',
+    borderRadius: scaleWidth(99),
+  },
+});
+
+export default SuccessBottomSheet;
