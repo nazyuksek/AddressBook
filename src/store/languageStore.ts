72e847d9449da3574a229db237e5735066b8ad86
naz <nyuksek17@ku.edu.tr>
2024-05-19 18:49:58 +0300

    chore: language store comment added

--- src/store/languageStore.ts
+++ src/store/languageStore.ts
@@ -1,6 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import languageReducer from './reducers/languageSlice';
 
+// Only used for keeping track of the current language for LanguageSelection component, but can be used for implementing custom localization in the future
 const languageStore = configureStore({
   reducer: languageReducer,
 });
